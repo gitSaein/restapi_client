f7583aafe6860d516b51f11bda8f6520481b04fa
USER <USER@192.168.13.111>
2018-12-27 14:39:02 +0900

    spring 개발 환경 setting

---
+++ src/main/java/common/api/ClimateRESTAPI.java
@@ -0,0 +1,20 @@
+package common.api;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLEncoder;
+import common.conf.Type;
+
+public class ClimateRESTAPI {
+
+	private String restClient(String city) throws Exception{
+		String serviceKey = URLEncoder.encode(Type.SERVICE_KEY.getValue(),"UTF-8");
+		String addr
+		= Type.REGION_REAL_API.getValue() + "?sidoName="+city+"&pageNo=1&numOfRows=10&ServiceKey=" + serviceKey + "&ver=1.3";
+		
+		URL url = new URL(addr);
+		InputStream in = url.openStream();
+		
+		return addr;
+	}
+}
---
+++ src/main/java/common/conf/ClimateApplication.java
@@ -0,0 +1,11 @@
+package common.conf;
+
+import org.springframework.boot.SpringApplication;
+
+public class ClimateApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ClimateApplication.class, args);
+	}
+}
