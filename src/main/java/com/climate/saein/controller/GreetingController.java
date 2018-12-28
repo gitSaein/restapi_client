a4a5298d4879402275f1d4448a2cb6c5125991af
USER <USER@192.168.13.111>
2018-12-28 13:34:20 +0900

    climate api호출 controller

--- src/main/java/com/climate/saein/controller/GreetingController.java
+++ src/main/java/com/climate/saein/controller/GreetingController.java
@@ -3,11 +3,20 @@ package com.climate.saein.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.climate.saein.common.api.RESTApi;
+
 @Controller
 public class GreetingController {
+	
+	RESTApi api = new RESTApi();
 
 	@GetMapping("/greeting")
 	public String greeting() {
+			try {
+				api.restClimateClient("¼­¿ï");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		return "greeting";
 	}
 	
