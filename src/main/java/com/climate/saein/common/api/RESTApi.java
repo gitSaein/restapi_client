91d86863aa5945a5a2002b9f1b2342a1e4a03c07
USER <USER@192.168.13.111>
2018-12-28 12:59:27 +0900

    Climate API 추가

--- src/main/java/com/climate/saein/common/api/RESTApi.java
+++ src/main/java/com/climate/saein/common/api/RESTApi.java
@@ -4,18 +4,26 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLEncoder;
 
+import org.apache.cxf.helpers.IOUtils;
+import org.apache.cxf.io.CachedOutputStream;
+
 import com.climate.saein.common.conf.Type;
 
-public class ClimateRESTAPI {
+public class RESTApi {
 
-	private String restClient(String city) throws Exception{
+	public String restClimateClient(String city) throws Exception{
 		String serviceKey = URLEncoder.encode(Type.SERVICE_KEY.getValue(),"UTF-8");
 		String addr
 		= Type.REGION_REAL_API.getValue() + "?sidoName="+city+"&pageNo=1&numOfRows=10&ServiceKey=" + serviceKey + "&ver=1.3";
-		
+		String data;
 		URL url = new URL(addr);
 		InputStream in = url.openStream();
-		
-		return addr;
+		CachedOutputStream cos = new CachedOutputStream();
+		IOUtils.copy(in, cos);
+		in.close();
+		cos.close();
+		data = cos.getOut().toString();
+		System.out.println("DATAs::" + cos.getOut().toString());
+		return data;
 	}
 }
